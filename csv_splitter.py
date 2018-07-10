2b99d516951fa6eb48f140a41df11da3a5889ec0
beomjin-lee <cluesbj@gmail.com>
2018-07-09 18:47:51 -0700

    Initial commit of csv_splitter

--- csv_splitter.py
+++ csv_splitter.py
@@ -18,48 +18,60 @@ https://gist.github.com/palewire/596056
 
 import os
 import csv
+import sys
+
 
 def split(file, delimiter = ',', row_limit = 10000,
-    output_name_template = 'output_%s.csv', output_path = '.',
-    keep_headers = True):
+          output_name_template = 'output_%s.csv', output_path = '.',
+          keep_headers = True):
+    """ Splits a CSV file into multiple pieces.
+
+    Arguments
+    ---------
+    `row_limit`: The number of rows you want in each output file. 10,000 by default.
+    `output_name_template`: A %s-style template for the numbered output files.
+    `output_path`: Where to stick the output files.
+    `keep_headers`: Whether or not to print the headers in each output file.
+
+    Example usage
+    -------------
+    >> from toolbox import csv_splitter;
+    >> csv_splitter.split(open('/home/brian_lee/input.csv', 'r'));
     """
-    Splits a CSV file into multiple pieces.
-
-    A quick bastardization of the Python CSV library.
-    Arguments:
-        `row_limit`: The number of rows you want in each output file. 10,000 by default.
-        `output_name_template`: A %s-style template for the numbered output files.
-        `output_path`: Where to stick the output files.
-        `keep_headers`: Whether or not to print the headers in each output file.
-    Example usage:
-
-        >> from toolbox import csv_splitter;
-        >> csv_splitter.split(open('/home/ben/input.csv', 'r'));
-
-    """
-    reader = csv.reader(file, delimiter = delimiter)
+    reader = csv.reader(file, delimiter=delimiter)
     current_piece = 1
     current_out_path = os.path.join(
-         output_path,
-         output_name_template  % current_piece
+        output_path,
+        output_name_template % current_piece
     )
-    current_out_writer = csv.writer(open(current_out_path, 'wb'), delimiter=delimiter)
+
+    current_out_writer = csv.writer(
+        open(current_out_path, 'wb'), delimiter = delimiter)
+
     current_limit = row_limit
+
     if keep_headers:
         headers = reader.next()
         current_out_writer.writerow(headers)
+
     for i, row in enumerate(reader):
         if i + 1 > current_limit:
             current_piece += 1
             current_limit = row_limit * current_piece
+
             current_out_path = os.path.join(
-               output_path,
-               output_name_template  % current_piece
+                output_path,
+                output_name_template % current_piece
             )
-            current_out_writer = csv.writer(open(current_out_path, 'wb'), delimiter = delimiter)
+
+            current_out_writer = csv.writer(
+                open(current_out_path, 'wb'), delimiter=delimiter)
+
             if keep_headers:
                 current_out_writer.writerow(headers)
+
         current_out_writer.writerow(row)
+
 
 if __name__ == "__main__":
     rfile = sys.argv[0]
@@ -68,4 +80,4 @@ if __name__ == "__main__":
     num_lines = sum(1 for line in open(rfile))
     split_lines = num_lines // num_files_split + 1
 
-    split(rfile, row_limit = split_lines)
+    split(rfile, row_limit=split_lines)
